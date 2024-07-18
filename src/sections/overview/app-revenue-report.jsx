6ca6237ebb851f03c94a84fff573a8e3921c8f85
bomismegg <minhtri28102003@gmail.com>
2024-07-19 06:29:17 +0700

    chart fix

--- src/sections/overview/app-revenue-report.jsx
+++ src/sections/overview/app-revenue-report.jsx
@@ -1,62 +1,37 @@
 import PropTypes from 'prop-types';
+import { BarChart, Bar, XAxis, YAxis, Legend, Tooltip, CartesianGrid, ResponsiveContainer } from 'recharts';
+import { Card, CardHeader } from '@mui/material';
 
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-
-import Chart, { useChart } from 'src/components/chart';
-
-export default function AppRevenueReport({ title, subheader, chart, ...other }) {
-  const { labels, series, options } = chart;
-
-  const chartOptions = useChart({
-    colors: ['#00AB55', '#FFC107', '#FF4842', '#007BFF'],
-    plotOptions: {
-      bar: {
-        columnWidth: '16%',
-      },
-    },
-    fill: {
-      type: series.map((i) => i.fill || 'solid'),
-    },
-    labels,
-    xaxis: {
-      type: 'category',
-    },
-    tooltip: {
-      shared: true,
-      intersect: false,
-      y: {
-        formatter: (value) => {
-          if (typeof value !== 'undefined') {
-            return `${value.toFixed(0)} VND`;
-          }
-          return value;
-        },
-      },
-    },
-    ...options,
-  });
+export default function AppRevenueReport({ data, title }) {
+  const formattedData = data.map((value, index) => ({
+    month: new Date(0, index).toLocaleString('default', { month: 'short' }),
+    revenue: value,
+  }));
+  console.log(formattedData)
 
   return (
-    <Card {...other}>
-      <CardHeader title={title} subheader={subheader} />
-      <Box sx={{ p: 3, pb: 1 }}>
-        <Chart
-          dir="ltr"
-          type="column"
-          series={series}
-          options={chartOptions}
-          width="100%"
-          height={364}
-        />
-      </Box>
+    <Card>
+      <CardHeader title={title} sx={{ mb: 5 }} />
+      <ResponsiveContainer width="100%" height={400}>
+        <BarChart
+          data={formattedData}
+          margin={{
+            top: 5, right: 30, left: 20, bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis dataKey="revenue" />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="revenue" fill="#458eff" />
+        </BarChart>
+      </ResponsiveContainer>
     </Card>
   );
 }
 
 AppRevenueReport.propTypes = {
-  chart: PropTypes.object.isRequired,
-  subheader: PropTypes.string,
-  title: PropTypes.string,
+  data: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired,
 };
